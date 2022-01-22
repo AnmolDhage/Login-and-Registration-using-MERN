eada98ee80905bed3114ce2a8b17ff957a7cb137
Anmol <anmoldhage999@gmail.com>
2022-01-22 10:30:06 +0530

    post request

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ const hbs = require("hbs");
 
 require("./db/conn");
 const Register = require("./models/registers")
-
 const port = process.env.PORT || 3000;
 
 // Paths start
@@ -14,9 +13,13 @@ const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
 // Paths end
 
+// to use post method and get input from form this is req
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//
 app.use(express.static(static_path));
 app.set("view engine", "hbs");
-app.set("views", template_path);
+app.set("views", template_path); //to change path of view folder
 hbs.registerPartials(partials_path)
 
 
@@ -33,6 +36,19 @@ app.get("/login", (req, res) => {
 })
 
 /*Get requests End*/
+/*post requests Start*/
+
+app.post("/register", async (req, res) => {
+  try {
+    console.log(req.body.first_name);
+    res.send(req.body.first_name);
+  }
+  catch {
+    response.status(400).send(error);
+  }
+})
+
+/*post requests End*/
 
 app.listen(port, () => {
   console.log(`server is up and running on ${port}`)
